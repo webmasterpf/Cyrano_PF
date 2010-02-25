6b01d9dcf370c66b5ece5187b67a9cf35cc9ffc6
webmasterpf-dg <webmaster@provence-formation.com>
2010-02-25 17:24:54 +0100

    maj du 25 Fev - page partenaires
    Signed-off-by: webmasterpf-dg <webmaster@provence-formation.com>

--- js/typos.js
+++ js/typos.js
@@ -1,7 +1,6 @@
-<script type="text/javascript">
 	$(document).ready(function() {
-	    if(!$.fontAvailable('Arista')) {
+	    if(!$.fontAvailable('Arista'))
+            {
 	      Cufon.replace('#contentPageActu h3.block-title');
-	    }
+              }
 	});
-</script>
